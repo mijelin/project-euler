674bba1ef8c812e999aeb5224e94f2a090d5267a
Michelle Lin <mj.lin@berkeley.edu>
2014-07-26 15:09:53 -0700

    Problem 1

---
+++ p001.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+/** MULTIPLES OF 3 AND 5
+ *  Find the sum of all the multiples of 3 or 5 below 1000.
+ *  @author Michelle Lin
+ */
+
+public class p001 {
+
+    private final static int MULT00 = 3;
+    private final static int MULT01 = 5;
+
+    private final static int UPPER_BOUND = 1000;
+
+    public static void main (String[] ignored) {
+        HashSet<Integer> nums = new HashSet<Integer>();
+        multiples(nums, MULT00, UPPER_BOUND);
+        multiples(nums, MULT01, UPPER_BOUND);
+        System.out.printf("Sum is: %d \n", sum(nums));
+    }
+
+    /** Adds all multiples of FACTOR [1, UPPERBOUND) to SET. */
+    private static void multiples(HashSet<Integer> set, int factor, int upperBound) {
+        for (int i = 1; i < upperBound; i += 1) {
+            if (i % factor == 0) { set.add(i); }
+        }
+    }
+
+    /** Returns the contents of all elements in SET. */
+    private static int sum(HashSet<Integer> set) {
+        int sum = 0;
+        if (set.isEmpty()) {
+            return sum;
+        } else {
+            Iterator<Integer> iter = set.iterator();
+            while (iter.hasNext()) {
+                sum += iter.next();
+            }
+            return sum;
+        }
+    }
+}
